9735362121c7c1deb9e729ee5ff341babebab633
M.A Jesowin Raja <soulcassiel@gmail.com>
2026-09-26 01:06:13 +0530

    feat: finish live system pages and settings

---
+++ app/s/[token]/page.tsx
@@ -0,0 +1,1 @@
+import{PublicShare}from"@/components/public/public-share";export default async function SharePage({params}:{params:Promise<{token:string}>}){const{token}=await params;return <PublicShare token={token}/>;}
