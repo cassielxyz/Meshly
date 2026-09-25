a4ccb56792ff6164cc272a1b1d80f4ccd46a40c8
M.A Jesowin Raja <soulcassiel@gmail.com>
2026-09-26 00:20:43 +0530

    fix: allow full-page OAuth endpoint navigation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
-export default defineConfig([...nextVitals,...nextTs,globalIgnores([".next/**","out/**","build/**","next-env.d.ts"])]);
+
+export default defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  {
+    rules: {
+      "@next/next/no-html-link-for-pages": "off",
+    },
+  },
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+]);
